26576ecff78545b0e00e327cb50d2cb66e64d619
yombi <ntwariegide2@gmail.com>
2021-04-23 22:20:24 +0200

    added data binding

--- src/app/course.component.ts
+++ src/app/course.component.ts
@@ -3,9 +3,11 @@ import {Component} from "@angular/core"
 //decorative function
 @Component({
     selector : 'courses', //<courses>
-    template : '<h2>My codes are running here now.</h2>' //html markups file or codes    
+    template : '<h2>{{ title }}</h2>' //html markups file or codes    
 })
 export class CoursesComponent { 
+    title = "My codes are running here now."
 }
 
 // importing to the module
+// working with templates , logic and component template
