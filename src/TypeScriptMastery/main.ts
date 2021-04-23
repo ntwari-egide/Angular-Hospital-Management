fd2aea42031a22fb197e35e9ef4f384495732443
yombi <ntwariegide2@gmail.com>
2021-04-23 21:00:45 +0200

    Variable declaring in angular

---
+++ src/TypeScriptMastery/main.ts
@@ -0,0 +1,8 @@
+let lawfirmName: String;
+let lawfirmAge: Number;
+let lawfirmNumberOfMembers: Number;
+let lawfirmValue: Boolean;
+let lawfirmMembers: String [] = ["Ntwari","Egide","Mugisha","Jules"]
+let lawfirmOthers: any[] = ["cow",2020,"kayonza",false]
+
+// Working with enums
