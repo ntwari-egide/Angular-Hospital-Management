fabde94fc43f69f68e2fb9cceab910cfbd6e3121
yombi <ntwariegide2@gmail.com>
2021-04-24 06:42:36 +0200

    Working  with services

--- src/app/course.component.ts
+++ src/app/course.component.ts
@@ -3,10 +3,16 @@ import {Component} from "@angular/core"
 //decorative function
 @Component({
     selector : 'courses', //<courses>
-    template : '<h2>{{ title }}</h2>' //html markups file or codes    
+    template : `
+        <h2>{{ title }}</h2> 
+        <ul>
+            <li *ngFor="let course of courses"> {{ course }}</li>
+        </ul>
+    ` //html markups file or codes    
 })
 export class CoursesComponent { 
     title = "My codes are running here now."
+    courses = ["course 1","course 2","course 3"]
 }
 
 // importing to the module
