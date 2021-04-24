92cf795618be966cbfc541f14e03b6e9b5fbb233
yombi <ntwariegide2@gmail.com>
2021-04-24 17:57:03 +0200

    Adding the content

--- src/app/angular-material-demo/angular-material-demo.component.ts
+++ src/app/angular-material-demo/angular-material-demo.component.ts
@@ -1,15 +1,65 @@
+import { state } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'angular-material-demo',
   templateUrl: './angular-material-demo.component.html',
   styleUrls: ['./angular-material-demo.component.css']
 })
-export class AngularMaterialDemoComponent implements OnInit {
 
-  constructor() { }
+export interface State {
+  flag : String
+  name: String
+  population: String
+}
 
-  ngOnInit(): void {
+export class AngularMaterialDemoComponent {
+  stateCtrl = new FormControl()
+  filteredStates: Observable<State[]>;
+
+  states: State[] = [
+    {
+      name: 'Arkansas',
+      population: '2.978M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
+    },
+    {
+      name: 'California',
+      population: '39.14M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
+    },
+    {
+      name: 'Florida',
+      population: '20.27M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
+    },
+    {
+      name: 'Texas',
+      population: '27.47M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
+    }
+  ];
+
+
+  constructor() { 
+    this.filteredStates = this.stateCtrl.valueChanges
+    .pipe(
+      startWith(''),
+      map(state => state ? this._filterStates(state) : this.states.slice())
+    );
   }
 
+  private _filterStates(value: string): State[] {
+    const filterValue = value.toLowerCase();
+
+    return this.states.filter(state => state.name.toLowerCase().indexOf(filterValue) === 0);
+  }
+  
 }
